94d1b3f6d843d0bef35409be2ad5c49b3d336c0f
Elias Levy <fearsome.lucidity@gmail.com>
2015-11-05 20:06:25 -0800

    Wait until we find the node before applying the labels.

--- apply-labels.sh
+++ apply-labels.sh
@@ -4,12 +4,21 @@ MD="curl -s http://169.254.169.254/latest/meta-data/"
 INSTANCE_ID=`${MD}/instance-id`
 INSTANCE_TYPE=`${MD}/instance-type`
 AVAILABILITY_ZONE=`${MD}/placement/availability-zone`
-NODE=`curl  -s \
-      --cert   /etc/kubernetes/ssl/worker.pem \
-      --key    /etc/kubernetes/ssl/worker-key.pem \
-      --cacert /etc/kubernetes/ssl/ca.pem  \
-      ${CONTROLLER_ENDPOINT}/api/v1/namespaces/default/pods/${HOSTNAME} | jq -r '.spec.nodeName'
-`
+
+# It appears it takes a while for the hostname to incorporate the node name.
+while [ -z "$NODE" ]; do
+  sleep 1
+  HOSTNAME=`hostname`
+  echo Hostname: $HOSTNAME
+  NODE=`curl  -s \
+        --cert   /etc/kubernetes/ssl/worker.pem \
+        --key    /etc/kubernetes/ssl/worker-key.pem \
+        --cacert /etc/kubernetes/ssl/ca.pem  \
+        ${CONTROLLER_ENDPOINT}/api/v1/namespaces/default/pods/${HOSTNAME} | jq -r '.spec.nodeName'
+  `
+done
+
+echo Node: $NODE
 
 curl  -s \
       --cert   /etc/kubernetes/ssl/worker.pem \
