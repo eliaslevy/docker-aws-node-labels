0a9cd439aa8897fbb0e9b4862f277e3a40a4aaff
Emmanuel Gomez <emmanuel@gomez.io>
2015-11-10 12:29:49 -0800

    Capture security groups as a comma-separated list.

--- apply-labels.sh
+++ apply-labels.sh
@@ -4,6 +4,7 @@ MD="curl -s http://169.254.169.254/latest/meta-data/"
 INSTANCE_ID=`${MD}/instance-id`
 INSTANCE_TYPE=`${MD}/instance-type`
 AVAILABILITY_ZONE=`${MD}/placement/availability-zone`
+SECURITY_GROUPS=`${MD}/security-groups | tr '\n' ','`
 
 # It appears it takes a while for the hostname to incorporate the node name.
 while [ "x$NODE" = "x" ] || [ "$NODE" = "null" ]; do
@@ -33,7 +34,8 @@ curl  -s \
     "labels": {
       "aws.node.kubernetes.io/id": "${INSTANCE_ID}",
       "aws.node.kubernetes.io/type": "${INSTANCE_TYPE}",
-      "aws.node.kubernetes.io/az": "${AVAILABILITY_ZONE}"
+      "aws.node.kubernetes.io/az": "${AVAILABILITY_ZONE}",
+      "aws.node.kubernetes.io/sgs": "${SECURITY_GROUPS}"
     } 
   } 
 }
